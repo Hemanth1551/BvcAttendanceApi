0c691e64565e22e33ceee1eaab659d46e878c7ef
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2025-07-22 18:24:59 +0530

    Update studentAttendanceRoutes.js

--- routes/studentAttendanceRoutes.js
+++ routes/studentAttendanceRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { addStudentAttendance, getAttendanceByStudentId, getStudentAttendanceMerged, getAllstudentattedancestoid, deleteattendance } = require('../controllers/studentAttendanceController');
+const { addStudentAttendance, getAttendanceByStudentId, getStudentAttendanceMerged, getAllstudentattedancestoid, deleteattendance, getAllstudenttodayattedances } = require('../controllers/studentAttendanceController');
 const router = express.Router();
 
 router.post('/addStudentattendance', addStudentAttendance);
 router.get('/student/:studentId', getAttendanceByStudentId); // ← NEW GET route
 router.get('/student/merged/:studentId', getStudentAttendanceMerged);
 router.get('/getAllstudentattedancestoid', getAllstudentattedancestoid); // ← NEW GET route
-
+router.get('/getAllstudenttodayattedances', getAllstudenttodayattedances);
 router.delete('/student/deleteatt/:stuId/:date', deleteattendance);
 
 
